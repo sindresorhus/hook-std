f2c706d706004e98df24813be621c91009be2222
André Costa Lima <andreclima.pt@gmail.com>
2017-07-27 23:36:59 +0100

    [BREAKING] Promisify the API (fixes #7)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function hook(type, opts, cb) {
+const hook = (type, opts, transform) => {
 	if (typeof opts !== 'object') {
-		cb = opts;
+		transform = opts;
 		opts = {};
 	}
 
@@ -11,39 +11,52 @@ function hook(type, opts, cb) {
 		once: false
 	}, opts);
 
-	const std = process[type];
-	const write = std.write;
+	let unhookFn;
 
-	const unhook = () => {
-		std.write = write;
+	const promise = new Promise(resolve => {
+		const std = process[type];
+		const write = std.write;
+
+		const unhook = () => {
+			std.write = write;
+			resolve();
+		};
+
+		std.write = (output, enc, cb) => {
+			const cbRet = transform(String(output), unhook);
+
+			if (opts.once) {
+				unhook();
+			}
+
+			if (opts.silent) {
+				return typeof cbRet === 'boolean' ? cbRet : true;
+			}
+
+			if (typeof cbRet === 'string') {
+				return write.call(std, Buffer.from(cbRet).toString(enc), enc, cb);
+			}
+
+			return write.call(std, Buffer.isBuffer(cbRet) ? cbRet : output, enc, cb);
+		};
+
+		unhookFn = unhook;
+	});
+
+	promise.unhook = unhookFn;
+
+	return promise;
+};
+
+module.exports = (opts, transform) => {
+	const stdoutPromise = hook('stdout', opts, transform);
+	const stderrPromise = hook('stderr', opts, transform);
+	const promise = Promise.all([stdoutPromise, stderrPromise]);
+	promise.unhook = () => {
+		stdoutPromise.unhook();
+		stderrPromise.unhook();
 	};
-
-	std.write = (str, enc, cb2) => {
-		const cbRet = cb(str, enc);
-
-		if (opts.once) {
-			unhook();
-		}
-
-		if (opts.silent) {
-			return typeof cbRet === 'boolean' ? cbRet : true;
-		}
-
-		const ret = Buffer.isBuffer(cbRet) || typeof cbRet === 'string' ? cbRet : str;
-		return write.call(std, ret, enc, cb2);
-	};
-
-	return unhook;
-}
-
-module.exports = (opts, cb) => {
-	const unhookStdout = hook('stdout', opts, cb);
-	const unhookStderr = hook('stderr', opts, cb);
-
-	return () => {
-		unhookStdout();
-		unhookStderr();
-	};
+	return promise;
 };
 
 module.exports.stdout = hook.bind(null, 'stdout');
